c1e531108a77eba71efd10c9710ffbf0ee896c51
EmilSchibli <emil.schibli@lernende.bbw.ch>
2024-07-11 09:10:03 +0200

    new

--- business-trip-app/client/src/App.js
+++ business-trip-app/client/src/App.js
@@ -10,27 +10,26 @@ const App = () => {
   return (
     <div className="app-container">
       <header className="header">
-        <h1>SchibliFlüge
-        </h1>
+        <h1>SchibliFlüge</h1>
       </header>
       <main className="App">
         <section className="hero">
-          <h2>Die Seite für Ihre Geschäftsreisen</h2>
-          <p>Entdecken Sie die besten Geschäftsreiseziele und buchen Sie Ihre Flüge, Hotels und Meetings ganz einfach online.</p>
+          <h2>Private Seite für Flüge der Familie Schibli</h2>
+          <p>Schibli Ferien Leicht gemacht.</p>
         </section>
         <section className="card-container">
           <div className="card">
-            <img src="san-francisco.jpg" alt="San Francisco" />
+            <img src="/images/san-francisco.jpg" alt="San Francisco" />
             <h3>Entdecken Sie San Francisco!</h3>
             <p>Als Herzstück der globalen Innovation bietet die Stadt einzigartige Geschäftsmöglichkeiten in Technologie, Finanzen und Biotechnologie.</p>
           </div>
           <div className="card">
-            <img src="santa-clara.jpg" alt="Santa Clara" />
+            <img src="/images/santa-clara.jpg" alt="Santa Clara" />
             <h3>Geschäftsmöglichkeiten in Santa Clara!</h3>
             <p>Im Herzen des Silicon Valley gelegen, bietet Santa Clara ideale Bedingungen für Innovation und Wachstum.</p>
           </div>
           <div className="card">
-            <img src="san-jose.jpg" alt="San José" />
+            <img src="/images/san-jose.jpg" alt="San José" />
             <h3>Erleben Sie das Wirtschaftszentrum San José!</h3>
             <p>Als Hauptstadt des Silicon Valley bietet San José eine Fülle an Geschäftsmöglichkeiten in Technologie, Finanzen und Innovation.</p>
           </div>
